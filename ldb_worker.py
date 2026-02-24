c42f0302bdfbdda429d8ef4f5a2b6a90aa46ebb9
bai-wenchao <wenchao.bai.chn@gmail.com>
2026-02-24 02:21:48 +0000

    Minor.

--- ldb_worker.py
+++ ldb_worker.py
@@ -14,10 +14,10 @@ class LdbWorker:
             f"in scenario: {self.workload.scenario}"
         ))
 
-        # Phase (1): Preprocessing
+        # Phase (1.1): Preprocessing
         self.workload.apply_sigma(debug=debug)
         for q_name, df in self.workload.sigma_satisfied_data.items():
             logger.info(f"Sigma retrieval for query '{q_name}' resulted in {len(df["data"].df)} rows.")
 
-        # Phase (1): Initialize the feature space.
+        # Phase (1.2): Initialize the feature space.
         await self.workload.init_coresets(enable_refinement=True, debug=debug)
